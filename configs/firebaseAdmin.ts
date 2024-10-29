4213de2134aa4c2229776d606714ad35b5f6c10b
Md Masud karim <msmasud578@gmail.com>
2024-10-29 15:54:43 +0100

    converting from file based firebase auth to env based

--- configs/firebaseAdmin.ts
+++ configs/firebaseAdmin.ts
@@ -1,12 +1,31 @@
 import "server-only";
 
-const { initializeApp, applicationDefault, cert,getApps } = require('firebase-admin/app');
+const { initializeApp, applicationDefault, cert,getApps,ServiceAcount } = require('firebase-admin/app');
+
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+
+
+
+const serviceAccount: ServiceAcount = {
+    authProviderX509CertUrl: process.env.auth_provider_x509_cert_url || '',
+    authUri: process.env.auth_uri || '',
+    clientEmail: process.env.client_email || '',
+    clientId: process.env.client_id || '',
+    clientX509CertUrl: process.env.client_x509_cert_url || '',
+    privateKey: process.env.private_key || '',
+    privateKeyId: process.env.private_key_id || '',
+    projectId: process.env.project_id || '',
+    tokenUri: process.env.token_uri || '',
+    type: process.env.type || '',
+    universeDomain: process.env.universe_domain || '',
+  };
+  
 
 console.log("Size of app :  "+getApps().length);
 if (getApps().length == 0) {
     initializeApp({
-        credential: cert("google.json")
+        // credential: cert("google.json")
+        credential : cert(serviceAccount)
     });
 }
 
