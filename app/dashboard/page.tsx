92f02aad759474225ab43aac6f6333e426f85d17
Md Masud karim <msmasud578@gmail.com>
2024-10-30 23:32:08 +0100

    a minor update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import UserCourseList from "./_components/UserCourseList";
 import { useUser } from '@clerk/clerk-react'
 
 
-const page = () => {
+const Page = () => {
   const { isSignedIn, user, isLoaded } = useUser()
   return (
     <div>
@@ -14,4 +14,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
