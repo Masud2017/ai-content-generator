a3a160c0a398a1b48b2adc777957c88f3a398ef4
Md Masud karim <msmasud578@gmail.com>
2024-10-31 23:50:44 +0100

    handled the private key as base64 string formate

--- configs/firebaseAdmin.ts
+++ configs/firebaseAdmin.ts
@@ -5,14 +5,15 @@ const { initializeApp, applicationDefault, cert,getApps,ServiceAcount } = requir
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
 
-
+let {private_key} = JSON.parse(process.env.private_key || "");
+private_key = private_key!.replace(/\\n/g, '\n');
 const serviceAccount = {
     authProviderX509CertUrl: process.env.auth_provider_x509_cert_url || '',
     authUri: process.env.auth_uri || '',
     clientEmail: process.env.client_email || '',
     clientId: process.env.client_id || '',
     clientX509CertUrl: process.env.client_x509_cert_url || '',
-    privateKey: process.env.private_key!.replace(/\\n/g, '\n') || '',
+    privateKey: private_key || '',
     privateKeyId: process.env.private_key_id || '',
     projectId: process.env.project_id || '',
     tokenUri: process.env.token_uri || '',
