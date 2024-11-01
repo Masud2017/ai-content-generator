cae9f7a77168bd2f038fcd24cb4246731dd9d7f0
Md Masud karim <msmasud578@gmail.com>
2024-11-01 14:38:48 +0100

    updated the env private key json format

--- configs/firebaseAdmin.ts
+++ configs/firebaseAdmin.ts
@@ -8,17 +8,29 @@ const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/
 let private_key = JSON.parse(process.env!.private_key || "{'private_key':'data'}")["private_key"];
 // private_key = private_key!.replace(/\\n/g, '\n');
 const serviceAccount = {
-    authProviderX509CertUrl: process.env.auth_provider_x509_cert_url || '',
-    authUri: process.env.auth_uri || '',
-    clientEmail: process.env.client_email || '',
-    clientId: process.env.client_id || '',
-    clientX509CertUrl: process.env.client_x509_cert_url || '',
-    privateKey: private_key || '',
-    privateKeyId: process.env.private_key_id || '',
-    projectId: process.env.project_id || '',
-    tokenUri: process.env.token_uri || '',
-    type: process.env.type || '',
-    universeDomain: process.env.universe_domain || '',
+    // authProviderX509CertUrl: process.env.auth_provider_x509_cert_url || '',
+    // authUri: process.env.auth_uri || '',
+    // clientEmail: process.env.client_email || '',
+    // clientId: process.env.client_id || '',
+    // clientX509CertUrl: process.env.client_x509_cert_url || '',
+    // privateKey: private_key || '',
+    // privateKeyId: process.env.private_key_id || '',
+    // projectId: process.env.project_id || '',
+    // tokenUri: process.env.token_uri || '',
+    // type: process.env.type || '',
+    // universeDomain: process.env.universe_domain || '',
+    'auth_provider_x509_cert_url': process.env.auth_provider_x509_cert_url || '',
+    'auth_uri': process.env.auth_uri || '',
+    'client_email': process.env.client_email || '',
+    'client_id': process.env.client_id || '',
+    'client_x509_cert_url': process.env.client_x509_cert_url || '',
+    'private_key': private_key || '',
+    'private_key_id': process.env.private_key_id || '',
+    'project_id': process.env.project_id || '',
+    'token_uri': process.env.token_uri || '',
+    'type': process.env.type || '',
+    'universe_domain': process.env.universe_domain || '',
+
   };
   
 
